b7633a3c04a2bc8c3e81ca1abba39b50ea9a5c89
Divyanshu <divyanshu12368@gmail.com>
2026-06-30 15:17:55 +0530

    Time: 1 ms (59.57%), Space: 48.9 MB (9.33%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] height) {
+        int left[] = new int[height.length];
+        int right[] = new int[height.length];
+        int trap = 0;
+        
+        //to calculate highest left baar array
+        left[0] = height[0];
+        for(int i = 1; i<left.length; i++){
+            left[i] = Math.max(left[i-1],height[i]);
+        }
+
+        //to calculate highest right baar/boundry array
+        right[right.length-1] = height[height.length-1];
+        for(int i = right.length-2; i>=0; i--){
+            right[i] = Math.max(right[i+1],height[i]);
+        }
+
+        // to calculate trap water for all bar
+        for(int i = 0; i<height.length; i++){
+            int mb = Math.min(left[i],right[i]);
+            trap += mb-height[i];
+        }
+
+        return trap;
+        
+    }
+}
