8257ec902aec73d364879c1235165be63bf7af7e
Divyanshu <divyanshu12368@gmail.com>
2026-07-16 16:19:05 +0530

    Time: 18 ms (13.58%), Space: 46.7 MB (9.17%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (map1.containsKey(ch)) {
+                map1.put(ch, map1.get(ch) + 1);
+            } else {
+                map1.put(ch, 1);
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (map2.containsKey(ch)) {
+                map2.put(ch, map2.get(ch) + 1);
+            } else {
+                map2.put(ch, 1);
+            }
+        }
+        if(map1.equals(map2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
