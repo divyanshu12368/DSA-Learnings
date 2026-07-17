5626d4a2d01f590062943748eaf7fea1ae822436
Divyanshu <divyanshu12368@gmail.com>
2026-07-17 16:49:32 +0530

    Time: 32 ms (30.18%), Space: 46.9 MB (72.92%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i = 0; i<s.length(); i++){
             char x = s.charAt(i);
             map.put(x, map.getOrDefault(x, 0) + 1);
+            continue;
         }
         for(int i = 0; i<s.length(); i++){
             char x = s.charAt(i);
