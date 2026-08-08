b8ceaa6daf774ff0d696b921c496190da537f91e
Divyanshu <divyanshu12368@gmail.com>
2026-08-08 19:38:35 +0530

    Time: 3 ms (24.62%), Space: 65 MB (75.79%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,18 +3,18 @@ class Solution {
         int size = nums.length;
         int[] answer = new int[size];
         int[] prefix = new int[size];
-        int[] postfix = new int[size];
-        postfix[size-1] = 1;
+        int[] suffix = new int[size];
+        suffix[size-1] = 1;
         prefix[0] = 1;
 
         for(int i = 1; i<size; i++){
             prefix[i] = prefix[i-1]*nums[i-1];
         }
         for(int j = size-2; j>=0; j--){
-            postfix[j] = postfix[j+1]*nums[j+1];
+            suffix[j] = suffix[j+1]*nums[j+1];
         }
         for(int i = 0; i<size; i++){
-            answer[i] = prefix[i]*postfix[i];
+            answer[i] = prefix[i]*suffix[i];
         }
         
         return answer;
